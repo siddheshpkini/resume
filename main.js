a3e0e365b36f54db72807ee531c7d7ef5642405d
siddheshpkini <siddheshpkini@gmail.com>
2021-08-13 04:36:55 +0530

    alignments adjustment for mobile view header

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const selectElement = (element) => document.querySelector(element);
 selectElement('.mobile-menu').addEventListener('click', () =>{
     selectElement('header').classList.toggle('active');
 });
+
+selectElement('#contactNumber').addEventListener('click', () =>{
+    navigator.clipboard.writeText('+916361986671');
+    alert('+916361986671 : Copied to clipboard');
+});
